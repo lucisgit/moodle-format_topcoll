0b58e35921bd7e9639479da3f3130eccbcd69e9d
Tony Butler <tonyjbutler@gmail.com>
2020-12-03 12:28:23 +0000

    Revert "Put back 'numsections' part 2."
    This reverts commit fae6abd64c3676f505e009b1ed1c747e9b504d89.

--- format.php
+++ format.php
@@ -67,8 +67,8 @@ if (($marker >= 0) && has_capability('moodle/course:setcurrentsection', $context
     course_set_marker($course->id, $marker);
 }
 
-// Make sure all sections are created.
-course_create_sections_if_missing($course, range(0, $course->numsections));
+// Make sure section 0 is created.
+course_create_sections_if_missing($course, 0);
 
 $renderer = $PAGE->get_renderer('format_topcoll');
 
